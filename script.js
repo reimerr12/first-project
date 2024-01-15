54975b3b390b52539ba494a7f0131de2f004e1cd
reimerr12 <sameermahirmomo@gmail.com>
2024-01-15 20:41:00 +0600

    html css and js files add

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("i am connected");
+
+var settingsmenu = document.querySelector("settings-menu");
+
+function settingsMenuToggle(){
+    settingsmenu.classList.toggle("settings-menu-height");
+}
